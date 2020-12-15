74fbb83938b01df7f1520d02348524937c86823f
8bitGiddyUp <lintcotton@gmail.com>
2020-12-15 16:58:03 -0500

    testing bind, call, apply, and arrow function

--- part2-colt/196Timer/196Timer.js
+++ part2-colt/196Timer/196Timer.js
@@ -8,8 +8,17 @@ class Timer{
     this.pauseButton.addEventListener('click', this.pause);
   }/* constructor */
 
-  start(){
+  start() {
+    console.log(this);
     console.log("time start button...");
+    /*
+    this fails
+    this.importantMethodToCall();
+    */
+  }
+
+  importantMethodToCall() {
+    console.log("i am an important method");
   }
 
   pause(){
@@ -23,3 +32,61 @@ const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
 const timer = new Timer(durationInput, startButton, pauseButton);
+//timer.start();
+
+/*
+  this -> dom window
+*/
+// console.log(this);
+// const printThis = () => {
+//   console.log(this);
+// };
+// printThis();
+
+
+
+
+
+
+// const colors = {
+//   printColor() {
+
+//     console.log("colors ->",this);
+//     const printThis = () => {
+//       console.log("colors ->", this);
+//     }
+
+//     printThis();
+//   }// printColor()
+// };
+
+// colors.printColor();
+
+
+
+
+// const printThis = function(){
+//   console.log(this);
+// }
+
+// printThis.call({color: 'red'});
+// printThis({color: 'blue'});
+// printThis();
+
+
+
+// const colors = {
+//   printColor(){
+//     console.log(this);
+//   }
+// };
+
+// const someObject = {
+//   a: 1
+// };
+
+// let pcolor = colors.printColor;
+// pcolor();
+
+// someObject.printColor = colors.printColor;
+// someObject.printColor();
