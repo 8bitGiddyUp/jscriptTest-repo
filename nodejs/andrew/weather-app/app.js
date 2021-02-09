a9fed78fd7d23dad50d4f659296d57ed25a26ae3
8bitGiddyUp <lintcotton@gmail.com>
2021-02-09 06:33:12 -0500

    testing promise

--- nodejs/andrew/weather-app/app.js
+++ nodejs/andrew/weather-app/app.js
@@ -1,11 +1,16 @@
-console.log("starting...");
+const request = require("request");
+const url =
+  "http://api.weatherapi.com/v1/forecast.json?key=e65f47728f0c4a52bbd142425210702&q=37.8267,-122.4233&days=3";
 
-setTimeout(() => {
-  console.log("fetching data...  2 second timer...");
-}, 2000);
+// "http://api.weatherapi.com/v1/forecast.json?key=e65f47728f0c4a52bbd142425210702&q=22204&days=3";
+//    https://swapi.dev/api/planets/
+//    http://api.weatherapi.com/v1/forecast.json?key=e65f47728f0c4a52bbd142425210702&q=37.8267,-122.4233&days=3
 
-setTimeout(() => {
-  console.log("0 second timer...");
-}, 0);
-
-console.log("stopping...");
+https: request({ url: url }, (error, response) => {
+  const wF = JSON.parse(response.body);
+  const currentTemp = wF.current.temp_f;
+  const rainChance = wF.forecast.forecastday[0].day.daily_chance_of_rain;
+  console.log(
+    `it is currently ${currentTemp} F.  There is a ${rainChance}% chance of rain.`
+  );
+});
