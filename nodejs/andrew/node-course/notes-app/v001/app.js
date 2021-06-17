e3b3b749c992b272fe225cd1973bc857ba0d91aa
8bitGiddyUp <lintcotton@gmail.com>
2021-06-17 09:19:47 -0400

    updates

--- nodejs/andrew/node-course/notes-app/v001/app.js
+++ nodejs/andrew/node-course/notes-app/v001/app.js
@@ -26,3 +26,18 @@ isURL = validator.isURL;
 // +++ chalk
 console.log(chalk.green("sucess"));
 // --- chalk
+
+// +++ nodemon
+console.log(chalk.red("ERROR"));
+// --- nodemon
+
+// +++ argv
+console.log(process.argv[2]);
+
+const command = process.argv[2];
+if (command === "add") {
+  console.log("adding notes");
+} else if (command === "remove") {
+  console.log("removing note");
+}
+// --- argv
