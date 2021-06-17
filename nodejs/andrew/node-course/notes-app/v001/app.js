de19e71fb1c6681305a4ae78e881cb5d390ac290
8bitGiddyUp <lintcotton@gmail.com>
2021-06-17 08:24:50 -0400

    npm modules

--- nodejs/andrew/node-course/notes-app/v001/app.js
+++ nodejs/andrew/node-course/notes-app/v001/app.js
@@ -1,5 +1,28 @@
-/* to use fs */
-const fs = require("fs");
+const validator = require("validator");
+//import validator from "validator";
 
-//fs.writeFileSync("notes.txt", "part 1: this file was by nodejs");
-fs.appendFileSync("notes.txt", "\ncontinue with new texts");
+const chalk = require("chalk");
+// import chalk from "chalk";
+
+//const utilName = require("./utils.js");
+const add2 = require("./utils.js");
+
+const getNotes = require("./notes.js");
+
+const sum = add2(2, 3);
+console.log("sum", sum);
+
+const msg = getNotes();
+console.log(msg);
+
+// +++ validator
+console.log(validator.isEmail("andyy.com"));
+console.log(validator.isURL("https://www.a.com"));
+isEmail = validator.isEmail("Hello.com");
+console.log("is email? ", isEmail);
+isURL = validator.isURL;
+// --- validator
+
+// +++ chalk
+console.log(chalk.green("sucess"));
+// --- chalk
